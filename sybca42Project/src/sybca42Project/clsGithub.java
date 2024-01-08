e3122b5a0fc9a9902ecfafb56dfbe67c522deac8
jenica mascarenhas <anaxmascarenhas12@gmail.com>
2024-01-08 09:47:21 +0530

    first github comit

---
+++ sybca42Project/src/sybca42Project/clsGithub.java
@@ -0,0 +1,12 @@
+package sybca42Project;
+
+public class clsGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
